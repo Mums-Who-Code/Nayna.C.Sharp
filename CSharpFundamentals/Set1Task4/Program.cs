6b3452f67b8f7105637d63345455dee19ede337b
NaynaMallick <naynamallick@outlook.com>
2022-03-21 21:00:23 -0700

    C# Fundamentals : Task 4

---
+++ CSharpFundamentals/Set1Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Set1Task3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 23;
+            int b = 7;
+            Program program = new Program();
+            int c1 = program.problem1(a, b);
+            Console.WriteLine("Value of a+++--b = " + c1);
+            int c2 = program.problem2(a, b);
+            Console.WriteLine("Value of ++a*(++b) = " + c2);
+        }
+
+        public int problem1(int a, int b)
+        {
+            int a1 = a++;
+            int b1 = --b;
+            return a1 + b1;
+        }
+
+        public int problem2(int a, int b)
+        {
+            int a1 = ++a;
+            int b1 = ++b;
+            return a1 * b1;
+        }
+    }
+}
