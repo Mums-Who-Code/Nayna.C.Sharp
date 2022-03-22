7ed981106b9dc41bb8b739614d9232e3148e42d1
NaynaMallick <naynamallick@outlook.com>
2022-03-21 21:10:05 -0700

    C# Fundamentals : Task 5

---
+++ CSharpFundamentals/Set1Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Set1Task5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime currentDateTime1 = DateTime.Now;
+            Console.WriteLine(currentDateTime1.ToString());
+            Console.WriteLine("Please enter a random integer number: ");
+
+            try
+            {
+                int days = int.Parse(Console.ReadLine());
+                DateTime newDateTime1 = currentDateTime1.AddDays(days);
+                Console.WriteLine(newDateTime1.ToString());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("You didn't entered a valid integer number. Try again.");
+            }
+        }
+    }
+}
