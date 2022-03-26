41e87a0bb795d474537044962916af10779b5582
NaynaMallick <naynamallick@outlook.com>
2022-03-26 08:03:53 -0700

    changed to static method

--- CSharpFundamentals/Set1Task4/Program.cs
+++ CSharpFundamentals/Set1Task4/Program.cs
@@ -6,38 +6,37 @@
         {
             int a = 23;
             int b = 7;
-            var program = new Program();
-            int c1 = program.problemAddition(a, b);
+            int c1 = problemAddition(a, b);
             Console.WriteLine("Value of (a++)+(--b) = " + c1);
-            int c2 = program.problemMultiply(a, b);
+            int c2 = problemMultiply(a, b);
             Console.WriteLine("Value of (++a)*(++b) = " + c2);
-            int c3 = program.problemSubstraction(a, b);
+            int c3 = problemSubstraction(a, b);
             Console.WriteLine("Value of (a++)-(--b) = " + c3);
-            int c4 = program.problemDivision(a, b);
+            int c4 = problemDivision(a, b);
             Console.WriteLine("Value of (++a)/(++b) = " + c4);
         }
 
-        public int problemAddition(int a, int b)
+        public static int problemAddition(int a, int b)
         {
             int a1 = a++;
             int b1 = --b;
             return a1 + b1;
         }
 
-        public int problemMultiply(int a, int b)
+        public static int problemMultiply(int a, int b)
         {
             int a1 = ++a;
             int b1 = ++b;
             return a1 * b1;
         }
 
-        public int problemSubstraction(int a, int b)
+        public static int problemSubstraction(int a, int b)
         {
             int a1 = a++;
             int b1 = --b;
             return a1 - b1;
         }
-        public int problemDivision(int a, int b)
+        public static int problemDivision(int a, int b)
         {
             int a1 = ++a;
             int b1 = ++b;
