2ffb86a9daacbbea67b1c5b48ff0a78b2830da56
NaynaMallick <naynamallick@outlook.com>
2022-03-26 21:22:32 -0700

    printed a and b

--- CSharpFundamentals/Set1Task4/Program.cs
+++ CSharpFundamentals/Set1Task4/Program.cs
@@ -8,10 +8,13 @@
             int b = 7;
             int c1 = problemAddition(a, b);
             Console.WriteLine("Value of (a++)+(--b) = " + c1);
+            Console.WriteLine();
             int c2 = problemMultiply(a, b);
             Console.WriteLine("Value of (++a)*(++b) = " + c2);
+            Console.WriteLine();
             int c3 = problemSubstraction(a, b);
             Console.WriteLine("Value of (a++)-(--b) = " + c3);
+            Console.WriteLine();
             int c4 = problemDivision(a, b);
             Console.WriteLine("Value of (++a)/(++b) = " + c4);
         }
@@ -20,6 +23,7 @@
         {
             int a1 = a++;
             int b1 = --b;
+            Console.WriteLine("Values of a++ and --b are " + a1 +" and " + b1);
             return a1 + b1;
         }
 
@@ -27,19 +31,22 @@
         {
             int a1 = ++a;
             int b1 = ++b;
+            Console.WriteLine("Values of ++a and ++b are " + a1 + " and " + b1);
             return a1 * b1;
         }
 
         public static int problemSubstraction(int a, int b)
         {
             int a1 = a++;
-            int b1 = --b;
+            int b1 = b++;
+            Console.WriteLine("Values of a++ and b++ are " + a1 + " and " + b1);
             return a1 - b1;
         }
         public static int problemDivision(int a, int b)
         {
             int a1 = ++a;
-            int b1 = ++b;
+            int b1 = b--;
+            Console.WriteLine("Values of ++a and b-- are " + a1 + " and " + b1);
             return a1 / b1;
         }
     }
